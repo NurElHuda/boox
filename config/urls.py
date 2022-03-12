7e1e2332347b89b08d6b98d76e9607b3fac855e0
Tine Nour el Houda <tinenourelhouda@gmail.com>
2022-03-12 20:58:16 +0100

    * feat: favico redirect

--- config/urls.py
+++ config/urls.py
@@ -17,9 +17,11 @@ from django.contrib import admin
 from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.views.generic.base import RedirectView
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('boox_app.urls'))
+    path('', include('boox_app.urls')),
+    path('favicon.ico', RedirectView.as_view(url="/static/favicon.ico", permanent=True)),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
