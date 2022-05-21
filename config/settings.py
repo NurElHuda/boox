a6f37e888799f1c1a18a160852cb3378ba701db8
Nour Tine <tinenourelhouda@gmail.com>
2022-05-22 00:50:16 +0100

    * settings (3)

--- config/settings.py
+++ config/settings.py
@@ -113,7 +113,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = "config.wsgi.application"
-if env("HOST", "LOCAL") == "DO":
+if env.get("HOST", "LOCAL") == "DO":
     DATABASES = {
         'default': env.db("DATABASE_URL")
     }
