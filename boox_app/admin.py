ec3b2ee007fb374cd9a09f45f27b0bb8c8a5c528
Nour Tine <tinenourelhouda@gmail.com>
2022-06-12 23:58:47 +0100

    * ypdate admin

--- boox_app/admin.py
+++ boox_app/admin.py
@@ -1,3 +1,67 @@
+# -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from .models import User, Book
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'password',
+        'last_login',
+        'is_superuser',
+        'username',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+        'date_joined',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'name',
+        'email',
+        'facebook',
+        'whatsapp',
+        'messenger',
+        'wilaya',
+        'is_admin',
+        'is_seller',
+    )
+    list_filter = (
+        'last_login',
+        'is_superuser',
+        'is_staff',
+        'is_active',
+        'date_joined',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'is_admin',
+        'is_seller',
+    )
+    raw_id_fields = ('groups', 'user_permissions')
+    search_fields = ('name',)
+    date_hierarchy = 'created_at'
+
+
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    list_display = (
+        'id',
+        'created_at',
+        'updated_at',
+        'deleted_at',
+        'seller',
+        'title',
+        'cover',
+        'author_name',
+        'wilaya',
+        'price',
+        'goodreads',
+        'messenger',
+        'whatsup',
+    )
+    list_filter = ('created_at', 'updated_at', 'deleted_at', 'seller')
+    date_hierarchy = 'created_at'
